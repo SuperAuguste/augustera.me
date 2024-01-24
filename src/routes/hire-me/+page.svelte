cd52f05145826c394b241562a32c0d29917014cb
SuperAuguste <19855629+SuperAuguste@users.noreply.github.com>
2024-01-23 21:01:21 -0400

    Improve phrasing

--- src/routes/hire-me/+page.svelte
+++ src/routes/hire-me/+page.svelte
@@ -6,10 +6,11 @@
 
 <p>
 	Since January 2024, I've run a sole proprietorship that combines my years of experience in (systems) programming and design as highlighted on my <a href="/resume">resume</a>
-	and <a href="/projects">projects showcase</a>.
+	and <a href="/projects">projects showcase</a>. I'd be excited to help you with a wide-ranging assortment of projects including but not limited to
+	websites/web applications, low-level libraries, compilers, and technical writing.
 </p>
 
 <p>
-	If you're interested, my starting rate, excepting non-profits and not-for-profit open-source projects, is $65 per hour.
-	You can contact me at <a href="mailto:auguste@augustera.me">auguste@augustera.me</a>.
+	If you're interested, you can contact me at <a href="mailto:auguste@augustera.me">auguste@augustera.me</a> and I'll happily give you a quote depending on the scope of your project.
+	Please let me know if you represent a non-profit or a not-for-profit open-source project.
 </p>
