eb86ce3b29234d24234274a5293fc223967d9698
SuperAuguste <19855629+SuperAuguste@users.noreply.github.com>
2024-01-26 11:07:07 -0400

    Adjust copy

--- src/routes/hire-me/+page.svelte
+++ src/routes/hire-me/+page.svelte
@@ -5,12 +5,5 @@
 <h2>Hire Me</h2>
 
 <p>
-	Since January 2024, I've run a sole proprietorship that combines my years of experience in (systems) programming and design as highlighted on my <a href="/resume">resume</a>
-	and <a href="/projects">projects showcase</a>. I'd be excited to help you with a wide-ranging assortment of projects including but not limited to
-	websites/web applications, low-level libraries, compilers, and technical writing.
-</p>
-
-<p>
-	If you're interested, you can contact me at <a href="mailto:auguste@augustera.me">auguste@augustera.me</a> and I'll happily give you a quote depending on the scope of your project.
-	Please let me know if you represent a non-profit or a not-for-profit open-source project.
+	If your interest has been piqued by anything on my <a href="/resume">resume</a> or <a href="/projects">projects showcase</a>, you can contact me at <a href="mailto:auguste@augustera.me">auguste@augustera.me</a>.
 </p>
