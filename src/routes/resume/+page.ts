f7044efb0273c35a57f423e03071fb189abf27ce
SuperAuguste <19855629+SuperAuguste@users.noreply.github.com>
2024-04-24 22:26:02 -0400

    Hopefully fix date issues on Webkit

--- src/routes/resume/+page.ts
+++ src/routes/resume/+page.ts
@@ -1,5 +1,10 @@
 import type { Resume } from "$lib/content";
 
+function dateFromMonthAndYear(date: string): Date {
+	const [month, year] = date.split(" ");
+	return new Date(`${month} 1, ${year}`);
+}
+
 export async function load() {
 	const resumeMarkdown = import.meta.glob("$lib/content/en/resume/**/*.md", { eager: true });
 	const about = resumeMarkdown["/src/lib/content/en/resume/about.md"];
@@ -26,9 +31,9 @@ export async function load() {
 
 	experience.sort((a, b) => {
 		if (a.meta.end === undefined && b.meta.end !== undefined) return -1;
-		return new Date(b.meta.start) - new Date(a.meta.start);
+		return dateFromMonthAndYear(b.meta.start) - dateFromMonthAndYear(a.meta.start);
 	});
-	talks.sort((a, b) => new Date(b.meta.date) - new Date(a.meta.date));
+	talks.sort((a, b) => dateFromMonthAndYear(b.meta.date) - dateFromMonthAndYear(a.meta.date));
 
 	const resume: Resume = {
 		about: {
